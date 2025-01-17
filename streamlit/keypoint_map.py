234bbc698b9c0fa52d308ee437151b70dd3567a7
june21a <june21r@gmail.com>
2025-01-17 11:56:55 +0900

    CP-32 update test note book and keypoint select method

--- streamlit/keypoint_map.py
+++ streamlit/keypoint_map.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+NUM_CLASSES = 33
 keypoint_mapping = {
     0: "nose",
     1: "left_eye_inner",
@@ -33,6 +36,7 @@ keypoint_mapping = {
     31: "left_foot_index",
     32: "right_foot_index"
 }
+SELECTED = [2, 5, 11, 12, 13, 14, 15, 16, 19, 20, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
 
 
 reverse_keypoint_mapping = {
@@ -71,6 +75,42 @@ reverse_keypoint_mapping = {
     "right_foot_index": 32
 }
 
+weights = {
+    "nose": 0.05,
+    "left_eye_inner": 0.02,
+    "left_eye": 0.02,
+    "left_eye_outer": 0.02,
+    "right_eye_inner": 0.02,
+    "right_eye": 0.02,
+    "right_eye_outer": 0.02,
+    "left_ear": 0.01,
+    "right_ear": 0.01,
+    "mouth_left": 0.02,
+    "mouth_right": 0.02,
+    "left_shoulder": 0.06,
+    "right_shoulder": 0.06,
+    "left_elbow": 0.04,
+    "right_elbow": 0.04,
+    "left_wrist": 0.03,
+    "right_wrist": 0.03,
+    "left_pinky": 0.01,
+    "right_pinky": 0.01,
+    "left_index": 0.01,
+    "right_index": 0.01,
+    "left_thumb": 0.01,
+    "right_thumb": 0.01,
+    "left_hip": 0.06,
+    "right_hip": 0.06,
+    "left_knee": 0.05,
+    "right_knee": 0.05,
+    "left_ankle": 0.04,
+    "right_ankle": 0.04,
+    "left_heel": 0.02,
+    "right_heel": 0.02,
+    "left_foot_index": 0.02,
+    "right_foot_index": 0.02
+}
+
 sigma_i_value = {
     'nose': 0.026,
     'eye': 0.025,
@@ -92,6 +132,26 @@ k_i_value = {
     k: sigma_i_value[k] for k in sigma_i_value.keys()
 }
 
+def normalize_landmarks(landmarks_np, mode="default"):
+    if mode == "shoulder":
+        right_shoulder_idx = reverse_keypoint_mapping['right_shoulder']
+        left_shoulder_idx = reverse_keypoint_mapping['left_shoulder']
+        d = np.linalg.norm(landmarks_np[right_shoulder_idx] - landmarks_np[left_shoulder_idx])
+    elif mode == 'l2':
+        d = np.linalg.norm(landmarks_np.flatten())
+    else:
+        right_hip_idx = reverse_keypoint_mapping['right_hip']
+        left_hip_idx = reverse_keypoint_mapping['left_hip']
+        hip_center = (landmarks_np[right_hip_idx] + landmarks_np[left_hip_idx]) / 2.
+        landmarks_np = landmarks_np - hip_center
+        right_shoulder_idx = reverse_keypoint_mapping['right_shoulder']
+        left_shoulder_idx = reverse_keypoint_mapping['left_shoulder']
+        d = np.linalg.norm((landmarks_np[right_shoulder_idx] - landmarks_np[left_shoulder_idx]))
+
+    return landmarks_np / d
+
+def filter_important_features(landmarks_np, targets=SELECTED):
+    return landmarks_np[targets]
 
 
 def landmarks_to_dict(all_landmarks):
