1517577b0569a7e7e1dbab28db4e676a5fa93d20
june21a <june21r@gmail.com>
2025-01-28 15:42:09 +0900

    CP-37 make challenge dl function

--- data_pipeline/ydl.py
+++ data_pipeline/ydl.py
@@ -38,6 +38,21 @@ def download_video(url, output_path, file_name):
     with YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])  # URL 리스트 전달
 
+def download_challenge(challenge_name, output_path, urls):
+    '''
+    challenge_name : 챌린지의 이름. 폴더별로 영상 구분하기 위해 설정
+    output_path : 비디오를 저장할 최상위 폴더. 비디오는 output_path/challenge_name/... 에 저장됨
+    urls : key값으로 right_video, wrong_videos 2가지를 가짐. right_video는 정답 video의 url/ wrong_videos는 비교대상들의 url list
+    '''
+    output_path = os.path.join(output_path, challenge_name)
+
+    right_video_url = urls['right_video']
+    download_video(right_video_url, output_path, f'right_video')
+
+    wrong_video_urls = urls['wrong_videos']
+    for idx, url in enumerate(wrong_video_urls):
+        download_video(url, output_path, f'wrong_video_{idx}')
+
 
 def main():
     right_video_url = urls['right_video']
