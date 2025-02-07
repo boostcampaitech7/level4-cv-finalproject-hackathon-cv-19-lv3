21033a826ef6731dac2d3ef3f7d3547f46173654
minrongtic <cocomong9997@naver.com>
2025-02-07 10:29:10 +0900

    posefixClone

--- backend/routers/feedback.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from models.model import FeedbackRequest
-from services.feedback import get_frame_feedback_service
-
-router = APIRouter()
-
-@router.post("/")
-async def get_frame_feedback(request: FeedbackRequest):
-    return await get_frame_feedback_service(request)
--- backend/routers/score.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from services.score import get_scores_service
-
-router = APIRouter()
-
-@router.get("/{folder_id}")
-async def get_scores(folder_id: str):
-    return await get_scores_service(folder_id)
